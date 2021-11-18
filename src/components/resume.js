787a58106b0c38bf55b56cc54feaabb6d641afe7
TRUDE135 <72237941+TRUDE135@users.noreply.github.com>
2021-11-18 16:37:08 -0600

    push

---
+++ src/components/resume.js
@@ -0,0 +1,45 @@
+export default function Resume() {
+  return (
+    <div className="content">
+      <div className="stuff">
+        <h1>Resume</h1>
+        <h2>Andrew Trudeau</h2>
+        <hr />
+        <p className="head">Interests</p>
+        <ul>
+          <li>Programming</li>
+          <li>Thinking about Programming</li>
+        </ul>
+        <p className="head">Skills</p>
+        <ul>
+          <li>Web Design with HTML, JavaScript, & CSS</li>
+          <li>Backend Web design with Node & Express</li>
+          <li>Testing Using Jest and Google Tests</li>
+          <li>Other Proficient Languages: C++, Java, C, C#, Python, R, MATLAB, OCaml, Haskell.</li>
+        </ul>
+        <p className="head">Education</p>
+        <ul>
+          <a href="https://twin-cities.umn.edu/">
+            <li>University of Minnesota Senior: CSCI</li>
+          </a>
+          <a href="https://bootcamp.umn.edu/">
+            <li>UMN Bootcamp</li>
+          </a>
+        </ul>
+        <p className="head">Experience</p>
+        <ul>
+          <li>Electrical Trade Jobs</li>
+          <li>Plenty of group-assigned work</li>
+          <li>More projects will be added but I am just filling this in to finish the assignment</li>
+        </ul>
+        <p className="head">Extracurricular</p>
+        <ul>
+          <li>Cooking</li>
+          <li>Music Production</li>
+          <li>Animal Care</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
